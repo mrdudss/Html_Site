399723141cc538b12420c5b6cf722979ce165c4f
Prathna_Seng <S.b.prathna@gmail.com>
2025-11-13 01:52:25 -0600

    part 2

--- src/filenode.py
+++ src/filenode.py
@@ -1,10 +1,11 @@
 from textnode import *
 import shutil
 import os
+import sys
 
 def search_dir(paths, level):
     src_root = "/root/repo/Html_Site/static"
-    dst_root = "/root/repo/Html_Site/public"
+    dst_root = "/root/repo/Html_Site/docs"
 
     rel = os.path.relpath(paths, src_root) 
     new_path = os.path.join(dst_root, rel)         
@@ -30,17 +31,16 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as file:
         file.write(final)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    src_root = "/root/repo/Html_Site/content"
-    dst_root = "/root/repo/Html_Site/public"
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath, content_root):
+    if basepath == None: basepath = "/"
 
-    rel = os.path.relpath(dir_path_content, src_root) 
-    new_path = os.path.join(dst_root, rel)         
+    rel = os.path.relpath(dir_path_content, content_root) 
+    new_path = os.path.join(dest_dir_path, rel)         
     os.makedirs(new_path, exist_ok=True)
     for items in os.listdir(dir_path_content):
         itempath = os.path.join(dir_path_content, items) 
         if os.path.isdir(itempath):
-            generate_pages_recursive(itempath, template_path, dest_dir_path)
+            generate_pages_recursive(itempath, template_path, dest_dir_path, basepath, content_root)
         elif os.path.isfile(itempath):
             print(f"Generating page from {dir_path_content} to {dest_dir_path} using {template_path}")
             with open(itempath) as f:
@@ -52,5 +52,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             title = extract_title(markdown)
             final = template.replace("{{ Title }}", title)
             final = final.replace("{{ Content }}", html)
-            with open(f"{new_path}/index.html", "w") as file:
+            final = final.replace('href="/', f'href="{basepath}')
+            final = final.replace('src="/', f'src="{basepath}')
+            with open(os.path.join(new_path, "index.html"), "w") as file:
                 file.write(final)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 from textnode import *
 from filenode import *
-import shutil, os, subprocess
+import shutil, os, subprocess, sys
 def main():
-    if os.path.exists("/root/repo/Html_Site/public"):
-        shutil.rmtree("/root/repo/Html_Site/public")
-    os.mkdir("/root/repo/Html_Site/public" ,mode=0o777)
+    basepath = sys.argv[1]
+    if os.path.exists("/root/repo/Html_Site/docs"):
+        shutil.rmtree("/root/repo/Html_Site/docs")
+    os.mkdir("/root/repo/Html_Site/docs" ,mode=0o777)
     for items in os.listdir("/root/repo/Html_Site/static"):
         paths = f"/root/repo/Html_Site/static/{items}"
         if os.path.isdir(paths):
             search_dir(paths, 6)
         elif os.path.isfile(paths):
-            shutil.copy(f"/root/repo/Html_Site/static/{items}", "/root/repo/Html_Site/public")
-    generate_pages_recursive("content", "template.html", "public/index.html")
+            shutil.copy(f"/root/repo/Html_Site/static/{items}", "/root/repo/Html_Site/docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath,"/root/repo/Html_Site")
     subprocess.run(["python3", "-m", "http.server", "8888"], cwd="public", check=True)
 main()
 
